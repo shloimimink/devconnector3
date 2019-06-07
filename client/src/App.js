0ff8a99eb2c7fdcc11c525d48b396b4ab15d1b7c
shloimi minkowicz <shloimimink@gmail.com>
2019-06-07 16:02:12 -0700

    react-client-starting

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import Landing from './components/layout/Landing';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = () =>
-    <Fragment>
-        <Navbar/>
-        <Landing/>
-    </Fragment>;
+    <Router>
+        <Fragment>
+            <Navbar/>
+            <Route exact path='/' component={Landing}/>
+            <section className="container">
+                <Switch>
+                    <Route exact path="/register" component={Register}/>
+                    <Route exact path="/login" component={Login}/>
+                </Switch>
+            </section>
+        </Fragment>
+    </Router>;
 export default App;
