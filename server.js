455df32a85fd3c8f3fb4006ce1954126914374c6
shloimi2 <shloimimink@gmail.com>
2019-08-12 09:34:19 -0500

    deployment

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
@@ -30,3 +25,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
