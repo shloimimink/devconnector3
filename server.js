414aeddc9bf3ff00858f5759232a14e2b27656cc
shloimi2 <shloimimink@gmail.com>
2019-08-12 08:34:49 -0500

    deployment

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.send('api Running'));
-
 // Define API Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
@@ -23,3 +21,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
