074ee2d581e926b86e78a0923424d3ff7c242fe6
shloimi2 <shloimimink@gmail.com>
2019-08-12 21:28:52 -0500

    deploy

---
+++ client/build/precache-manifest.3298a2ef50a444c4315f4be93b9fa107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08701f991563c39929ca0da5ef979696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbd6ce9cca8718752bd2",
+    "url": "/static/css/main.53395aa5.chunk.css"
+  },
+  {
+    "revision": "b037a1dcc1ffad7e4775",
+    "url": "/static/js/2.acc71c6f.chunk.js"
+  },
+  {
+    "revision": "fbd6ce9cca8718752bd2",
+    "url": "/static/js/main.62fac6db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
