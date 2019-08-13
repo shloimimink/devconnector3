24be5e55ad09512af9b1b3a7e8e519f6f5210436
shloimi2 <shloimimink@gmail.com>
2019-08-13 12:17:49 -0500

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Landing from "./components/layout/Landing";
 import Routes from './components/routing/Routes';
 
 // Redux
@@ -9,9 +10,7 @@ import store from './store';
 import {loadUser} from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 
-
 import './App.css';
-import Landing from "./components/layout/Landing";
 
 if (localStorage.token) {
     setAuthToken(localStorage.token)
