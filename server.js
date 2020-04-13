53b9a4891aba3f92aa67a9474b03945401b6fc75
shloimimink <shloimimink@gmail.com>
2020-04-12 20:46:26 -0500

    warning fixes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
@@ -16,6 +17,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/profile', require('./routes/profile'));
 app.use('/api/posts', require('./routes/posts'));
+
 
 
 // Serve static assets in productionheroku open
